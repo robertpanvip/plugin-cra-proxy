c069bb0f8aceebfe7d5bd338fea8e1770f846f45
robertpanvip <robertpanvip@163.com>
2025-11-29 16:07:24 +0800

    feat:init

--- packages/rsbuild/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { type PluginCraProxyOptions } from '@plugin-cra-proxy/core';
-import type { RsbuildPlugin } from '@rsbuild/core';
-export declare const pluginCraProxy: (options?: PluginCraProxyOptions) => RsbuildPlugin;
-export default pluginCraProxy;
